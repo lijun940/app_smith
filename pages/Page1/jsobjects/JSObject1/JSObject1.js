a920909f11f20ecc69114ed1048dacf15c7b5fc6
lijun940 <lijun940>
2023-05-25 07:36:04 +0000

    feat: 新增点击开发弹窗功能

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,15 @@
+export default {
+	showDialog: false,
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		this.showDialog = true
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
